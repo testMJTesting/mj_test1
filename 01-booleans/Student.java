4401e1decb671ff02721a4e12b70ffa7d5a6fc32
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 17:07:58 +0100

    t

--- 01-booleans/Student.java
+++ 01-booleans/Student.java
@@ -2,4 +2,10 @@ class Student {
   // Implement the function 'static boolean inInterval(x, y, z)' that
   // returns true if x is higher than or the same as y and lower than or the same as z
   // returns false if not
+public boolean inInterval(int x, int y, int z){
+	return x >= y && x <= z;
+}
+
+
+
 }
