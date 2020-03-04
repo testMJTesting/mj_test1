59d2c49e41a03e901092a4f6bf03e12cae265858
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-04 17:08:08 +0100

    New question from category 02-conditionals

--- 02-conditionals/Student.java
+++ 02-conditionals/Student.java
@@ -1,15 +1,6 @@
-public class Student {
+class Student {
     // Je wil t-shirts laten drukken.
     // Per t-shirt betaal je €10.
     // Als je 10 of meer t-shirts laat drukken, krijg je €5 korting.
     // Schrijf een functie tShirts(n) die, gegeven een aantal t-shirts n, teruggeeft hoeveel je moet betalen.
-
-public int tShirts(int n){
-	if (n < 10){
-		return n * 10;
-	}else{
-		return n*10 - 5;
-	}
-}
-
 }
