a51f23250223a7e7bc27fab67ed560b6e6b30175
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 17:12:44 +0100

    t

--- 03-arrays/Student.java
+++ 03-arrays/Student.java
@@ -1,4 +1,10 @@
 class Student {
     // Schrijf een functie last(int[] xs) die gegeven een array xs het laatste element ervan teruggeeft.
     // Je mag ervan uitgaan dat xs minstens één element bevat.
+
+public int last(int[] xs){
+
+	return xs[xs.length - 1]
+}
+
 }
