329bd47741135d707e48e82644906efc866643ff
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-04 16:32:18 +0100

    New question from category 01-booleans

--- 01-booleans/Student.java
+++ 01-booleans/Student.java
@@ -2,7 +2,4 @@ class Student {
   // Implement the function 'static boolean isPositive(int)'
   // returns true if the integer is positive (0 or greater)
   // returns false if not
-public boolean isPositive(int in){
-	return in >= 0;
-}
 }
