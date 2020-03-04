e876cafbd9e737b468e5a26e492cefecc6e67c7b
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 16:48:53 +0100

    boolean test

--- 01-booleans/Student.java
+++ 01-booleans/Student.java
@@ -2,4 +2,9 @@ class Student {
   // Implement the function 'static boolean isPositive(int)'
   // returns true if the integer is positive (0 or greater)
   // returns false if not
+public boolean isPositive(int in){
+	return in >= 0;
+}
+
+
 }
