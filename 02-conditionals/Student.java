e2ddccd67b9a8ff6c82037a9c8d76ff2c95f82b2
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 17:10:04 +0100

    t

--- 02-conditionals/Student.java
+++ 02-conditionals/Student.java
@@ -3,4 +3,13 @@ class Student {
     // Per t-shirt betaal je €10.
     // Als je 10 of meer t-shirts laat drukken, krijg je €5 korting.
     // Schrijf een functie tShirts(n) die, gegeven een aantal t-shirts n, teruggeeft hoeveel je moet betalen.
+
+public int tShirts(int n){
+	if (n < 10) {
+		return n * 10;
+	}else{
+		return n * 10 - 5;
+	}
+}
+
 }
