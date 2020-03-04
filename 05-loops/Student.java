18690e8ff538803ccfde08255308c2efaf0c614f
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-04 17:19:13 +0100

    New question from category 05-loops

---
+++ 05-loops/Student.java
@@ -0,0 +1,10 @@
+class Student {
+
+    // LOOPS ==============================================
+
+    // Schrijf een functie sumRange(a, b) die, gegeven twee gehele getallen a en b,
+    // de som van a tot en met b uitrekent:
+    //
+    // a + (a + 1) + (a + 2) + ... + b
+
+}
