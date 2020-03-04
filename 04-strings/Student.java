6dbc369a7bcffaa421330fb140033f12d448efee
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-04 17:17:36 +0100

    New question from category 04-strings

---
+++ 04-strings/Student.java
@@ -0,0 +1,10 @@
+class Student {
+
+    // STRINGS ==============================================
+
+    // Schrijf een functie "String repeat(String str, int n)" die een nieuwe string construeert
+    // door n keer de string str te herhalen. Je mag ervan uitgaan dat n niet negatief is.
+    //
+    // E.g. repeat("abc", 3) returns "abcabcabc"
+    
+}
